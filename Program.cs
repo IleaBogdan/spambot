fad8648c6a5845aafa7654d8d727306f6daed37b
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-11-04 20:51:38 +0200

    repeat time added

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace spambot
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mesaj ce va fi spamat:");
             string message=Console.ReadLine();
+            Console.WriteLine("De cate ori va fi spamat:");
+            int repeat=Convert.ToInt32(Console.ReadLine());
             Thread.Sleep(5000);
-            for (int i=0; i<250; i++) 
+            for (int i=0; i<repeat; i++) 
             {
                 for (int j=0; j<message.Length; j++)
                 {
